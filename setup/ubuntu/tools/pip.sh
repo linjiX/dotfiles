e64b4446338ecab97375caf9bfef8ce460f298dd
linji.xue <linji.xue@graviti.cn>
2020-02-26 19:30:02 +0800

    Use 'requirements.txt' for pip

--- setup/ubuntu/tools/pip.sh
+++ setup/ubuntu/tools/pip.sh
@@ -9,31 +9,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-# NeoVim
-pip3 install neovim
-
-# Git
-pip3 install gitlint
-
-# Python
-pip3 install \
-    black \
-    isort \
-    ipython \
-    pylint \
-    flake8 \
-    mypy
-
-# C/C++
-pip3 install cpplint
-
-# Json
-pip3 install demjson
-
-# cmake
-pip3 install \
-    cmake_format \
-    cmakelint
-
-# VimScript
-pip3 install vim-vint
+pip3 install -r requirements.txt
