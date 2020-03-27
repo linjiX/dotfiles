d702406f7141320c961aa52e06fb10381957f680
linji.xue <linji.xue@graviti.cn>
2020-03-27 19:04:05 +0800

    Remove 'openjdk-8-jdk' dependency from 'install_bazel.sh'

--- setup/ubuntu/tools/install_bazel.sh
+++ setup/ubuntu/tools/install_bazel.sh
@@ -9,14 +9,14 @@ set -v
 # Install Bazel #
 #################
 
-if ! dpkg -s openjdk-8-jdk curl 1>/dev/null 2>&1; then
+if ! dpkg -s curl 1>/dev/null 2>&1; then
     sudo apt-get update
-    sudo apt-get install -y openjdk-8-jdk curl
+    sudo apt-get install -y curl
 fi
 
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" |
     sudo tee /etc/apt/sources.list.d/bazel.list
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 
 sudo apt-get update
 sudo apt-get install -y bazel
