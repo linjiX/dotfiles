2e69fde8ae8a39e48a361c20b35dcc4f2147c108
linji.xue <linji.xue@graviti.cn>
2020-05-06 19:54:31 +0800

    [setup] Check 'curl' in 'nodejs' installation script

--- setup/ubuntu/tools/install_node.sh
+++ setup/ubuntu/tools/install_node.sh
@@ -3,7 +3,11 @@
 set -euo pipefail
 set -x
 
-# https://github.com/nodesource/distributions#debinstall
+if ! dpkg -s curl 1>/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y curl
+fi
 
+# https://github.com/nodesource/distributions#debinstall
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
