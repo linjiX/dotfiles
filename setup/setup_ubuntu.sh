58b543da3b8b7439dba60d21a8ddfbf4873413d6
linji.xue <linji.xue@graviti.cn>
2020-05-17 17:30:54 +0800

    [setup] Adjust the installation order following the 'Dockerfile'

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -11,13 +11,13 @@ ln -sf ~/.config/dotfiles/gitconfig ~/.gitconfig
 ./ubuntu/common/basic.sh
 ./ubuntu/common/advanced.sh
 ./ubuntu/tools/install_gcc.sh
+./ubuntu/tools/install_node.sh
+./ubuntu/tools/install_pyenv.sh
 ./ubuntu/tools/install_tmux.sh
 ./ubuntu/tools/install_arcanist.sh
 ./ubuntu/tools/install_fzf.sh
 ./ubuntu/tools/install_bazel.sh
 ./ubuntu/tools/install_bazelisk.sh
-./ubuntu/tools/install_node.sh
-./ubuntu/tools/install_pyenv.sh
 ./ubuntu/customize/vim.sh
 ./ubuntu/tools/pip.sh
 ./ubuntu/tools/npm.sh
