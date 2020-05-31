1fc3821f8050d052a6440bcde75760455696d779
linji.xue <linji.xue@graviti.cn>
2020-06-01 01:21:00 +0800

    [setup] Chmod +x 'setup.sh' & Use 'setup.sh' in 'dotfiles.sh'

--- setup/ubuntu/customize/dotfiles.sh
+++ setup/ubuntu/customize/dotfiles.sh
@@ -4,7 +4,4 @@ set -euo pipefail
 set -x
 
 git clone --depth=1 https://github.com/linjiX/dotfiles.git ~/.config/dotfiles/
-
-echo 'source ~/.config/dotfiles/bash/bashrc' >> ~/.bashrc
-ln -sf ~/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -sf ~/.config/dotfiles/git/gitconfig ~/.gitconfig
+~/.config/dotfiles/setup.sh
