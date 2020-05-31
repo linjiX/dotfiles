23ace8bb049914f4a4ee83216e58d48dcc99103e
linji.xue <linji.xue@graviti.cn>
2020-06-01 00:00:35 +0800

    [setup] Adjust 'dotfiles.sh' due to refactor: 1d48ac9

--- setup/ubuntu/customize/dotfiles.sh
+++ setup/ubuntu/customize/dotfiles.sh
@@ -5,6 +5,6 @@ set -x
 
 git clone --depth=1 https://github.com/linjiX/dotfiles.git ~/.config/dotfiles/
 
-echo 'source ~/.config/dotfiles/bashrc' >> ~/.bashrc
-ln -sf ~/.config/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/.config/dotfiles/gitconfig ~/.gitconfig
+echo 'source ~/.config/dotfiles/bash/bashrc' >> ~/.bashrc
+ln -sf ~/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/.config/dotfiles/git/gitconfig ~/.gitconfig
