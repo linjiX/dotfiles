99870c524b0585df9b2044aa455d88347c3250f4
linji.xue <linji.xue@graviti.cn>
2020-11-22 15:06:50 +0800

    [setup] Add config softlink & system check in 'setup_ubuntu.sh'

--- setup/setup_ubuntu.sh
+++ setup/setup_ubuntu.sh
@@ -4,16 +4,32 @@ pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
 set -euo pipefail
 set -x
 
-echo 'source ~/.config/dotfiles/bashrc' >> ~/.bashrc
-ln -sf ~/.config/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/.config/dotfiles/gitconfig ~/.gitconfig
+[ -r /etc/lsb-release ] && source /etc/lsb-release
+
+echo 'source ~/.config/dotfiles/bash/bashrc' >>~/.bashrc
 
 ./ubuntu/common/basic.sh
 ./ubuntu/common/advanced.sh
-./ubuntu/tools/install_gcc.sh
+
+# git
+ln -sf ~/.config/dotfiles/git/gitconfig ~/.gitconfig
+
+# tmux
+ln -sf ~/.config/dotfiles/tmux/tmux.conf ~/.tmux.conf
+git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth=1 https://github.com/tmux-plugins/tmux-cpu ~/.tmux/plugins/tmux-cpu
+
+# ranger config
+git clone --depth=1 https://github.com/alexanderjeurissen/ranger_devicons \
+    ~/.config/ranger/plugins/ranger_devicons
+ln -sf ~/.config/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
+
+if [ "$DISTRIB_CODENAME" == 'xenial' ]; then
+    ./ubuntu/tools/install_gcc.sh
+fi
+
 ./ubuntu/tools/install_node.sh
 ./ubuntu/tools/install_pyenv.sh
-./ubuntu/tools/install_tmux.sh
 ./ubuntu/tools/install_arcanist.sh
 ./ubuntu/tools/install_fzf.sh
 ./ubuntu/tools/install_bazel.sh
