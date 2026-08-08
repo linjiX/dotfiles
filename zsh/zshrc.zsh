4de60d1dd3a2f5528f401dd78f4b754dda643bb2
linji.xue <linji.xue@graviti.com>
2026-08-08 21:14:47 +0800

    fix(zsh): deduplicate PATH entries

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -16,6 +16,8 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+typeset -U path PATH
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
